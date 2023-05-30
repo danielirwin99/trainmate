963be067e0858100e6b9c1e523387a9aa8758807
Daniel Irwin <danielirwin99@gmail.com>
2023-05-30 12:00:42 +1000

    Building

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -5,7 +5,20 @@ const Profile = () => {
   return (
     <>
       <Navbar />
-      <div className="pageHeight"></div>
+      <div className="pageHeight w-full">
+        <div className="flex">
+          <figure className="w-30% h-full">
+            <img
+              className="w-full h-full"
+              src="https://www.pngall.com/wp-content/uploads/2017/05/TM-Symbol-PNG-Picture.png"
+              alt=""
+            />
+          </figure>
+          <div>
+            <h1>John Doe</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
